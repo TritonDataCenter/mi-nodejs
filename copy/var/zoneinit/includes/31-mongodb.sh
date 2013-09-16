4ab5fae02877b86a6026f60e074850027bc570f0
Derek Crudgington <derek@joyent.com>
2013-09-16 13:46:51 +0000

    Added version 13.2.0

--- copy/var/zoneinit/includes/31-mongodb.sh
+++ copy/var/zoneinit/includes/31-mongodb.sh
@@ -1,57 +1,63 @@
-# Get MongoDB password or set one.
+# Get password from metadata, unless passed as MONGODB_PW, or set one.
 log "getting mongodb_pw"
-MONGODB_PW=$(mdata-get mongodb_pw 2>/dev/null) || MONGODB_PW=$(od -An -N8 -x /dev/random | head -1 | tr -d ' ');
+MONGODB_PW=${MONGODB_PW:-$(mdata-get mongodb_pw 2>/dev/null)} || \
+MONGODB_PW=$(od -An -N8 -x /dev/random | head -1 | tr -d ' ');
 
-# Generate quickbackup-mongodb passowrd
-# (svccfg doesn't accept special characters).
+# Get Quickbackup user and password
 log "getting qb_pw"
-QB_PW=$(od -An -N8 -x /dev/random | head -1 | tr -d ' ');
+QB_PW=$(od -An -N8 -x /dev/random | head -1 | sed 's/^[ \t]*//' | tr -d ' ');
 QB_US=qb-$(zonename | awk -F\- '{ print $5 }');
 
+# Start MongoDB
 log "starting the MongoDB instance"
-svcadm enable mongodb
+svcadm enable pkgsrc/mongodb
 
+# Wait for MongoDB to start
 log "waiting for the socket to show up"
+COUNT="0";
 while [[ ! -e /tmp/mongodb-27017.sock ]]; do
-	: ${MYCOUNT:=0}
 	sleep 1
-	((MYCOUNT=MYCOUNT+1))
-	if [[ $MYCOUNT -eq 30 ]]; then
-	  log "ERROR Could not talk to MongoDB after 30 seconds"
+	((COUNT=COUNT+1))
+	if [[ ${COUNT} -eq 60 ]]; then
+	  log "ERROR Could not talk to MongoDB after 60 seconds"
           ERROR=yes
           break 1
         fi
 done
 [[ -n "${ERROR}" ]] && exit 31
-log "(it took ${MYCOUNT} seconds to start properly)"
+log "(it took ${COUNT} seconds to start properly)"
+sleep 1
 
-sleep 1;
-
-[[ "$(svcs -Ho state mongodb:default)" == "online" ]] || \
+# Check if MongoDB service is online
+[[ "$(svcs -Ho state pkgsrc/mongodb)" == "online" ]] || \
   ( log "ERROR MongoDB SMF not reporting as 'online'" && exit 31 )
 
+# Configure MongoDB password
 log "Setting the MongoDB admin password"
-mongo 127.0.0.1/admin --eval "db.addUser(\"admin\", \"${MONGODB_PW}\")" 2>/dev/null || \
+/opt/local/bin/mongo 127.0.0.1/admin --eval "db.addUser(\"admin\", \"${MONGODB_PW}\")" 2>/dev/null || \
   ( log "ERROR MongoDB set admin pass failed to execute." && exit 31 )
+sleep 2;
 
-sleep 1;
-
+# Configure MongoDB Quickbackup user
 log "Setting the MongoDB qb password"
-mongo 127.0.0.1/admin -uadmin -p${MONGODB_PW} --eval "db.addUser(\"${QB_US}\", \"${QB_PW}\")" 2>/dev/null || \
+/opt/local/bin/mongo 127.0.0.1/admin -uadmin -p${MONGODB_PW} --eval "db.addUser(\"${QB_US}\", \"${QB_PW}\")" 2>/dev/null || \
   ( log "ERROR MongoDB set qb pass failed to execute." && exit 31 )
 
+# Configure MongoDB authentication
 log "putting auth in mongodb.conf and starting mongodb"
 echo "auth = true" >> /opt/local/etc/mongodb.conf
 
+# Configure MongoDB Quickbackup service
 log "configuring quickbackup-mongodb with the proper user/pass"
-svccfg -s quickbackup-mongodb setprop quickbackup/username = astring: ${QB_US}
-svccfg -s quickbackup-mongodb setprop quickbackup/password = astring: ${QB_PW}
+svccfg -s pkgsrc/quickbackup-mongodb setprop quickbackup/username = astring: ${QB_US}
+svccfg -s pkgsrc/quickbackup-mongodb setprop quickbackup/password = astring: ${QB_PW}
 svcadm refresh quickbackup-mongodb
 
-log "clearing mongodb logs"
-svcadm disable mongodb:default
+# Disable MongoDB, clear logs, enable MongoDB
+log "stopping mongodb and clearing logs"
+svcadm disable pkgsrc/mongodb
 rm /var/log/mongodb/mongodb.log
 touch /var/log/mongodb/mongodb.log
 chown -R mongodb:mongodb /var/log/mongodb
 chown -R mongodb:mongodb /var/mongodb
-svcadm enable mongodb:default
+svcadm enable pkgsrc/mongodb
